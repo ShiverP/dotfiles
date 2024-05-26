7ec72a0a2daaa33481bb2700f19b09bda7384748
ShiverP <shivamparwat01@gmail.com>
2024-05-26 22:00:03 +0200

    Added gh-cli, removed foot

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Update package lists
-pacman -Syu
+sudo pacman -Syu
 
 # Install essential packages
-pacman -S git neovim zsh fzf base-devel alacritty dolphin curl wget wofi stow ttf-font-awesome nerd-fonts --noconfirm
+sudo pacman -S git github-cli neovim zsh fzf base-devel alacritty dolphin curl wget wofi stow ttf-font-awesome nerd-fonts --noconfirm 
+
+# Uninstall redundant stuff
+sudo pacman -Rs foot --noconfirm
 
 # Configure shell
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh
