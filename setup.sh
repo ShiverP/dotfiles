db4a25e8811d22ba67c011df5f1e20497f189f88
ShiverP <shivamparwat01@gmail.com>
2024-05-26 21:57:35 +0200

    Added fonts to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 pacman -Syu
 
 # Install essential packages
-pacman -S git neovim zsh fzf base-devel alacritty dolphin curl wget wofi stow
+pacman -S git neovim zsh fzf base-devel alacritty dolphin curl wget wofi stow ttf-font-awesome nerd-fonts --noconfirm
 
 # Configure shell
 chsh -s /usr/bin/zsh
