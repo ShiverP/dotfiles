f52b0dc7210f11939467699096c8f2a273d4c807
ShiverP <shivamparwat01@gmail.com>
2024-05-26 23:08:34 +0200

    Added extra config to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Go into root
+cd /
+
+# Add lines to sudoers and pacman configs
+sudo echo "[options]
+ILoveCandy
+Color
+ParallelDownloads = 25" >> /etc/pacman.conf
+
+sudo touch /etc/sudoers.d/10-arch
+sudo echo "shiver ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/10-arch 
+
+# cd back to user home
+cd
+
 # Update package lists
 sudo pacman -Syu
 
@@ -8,6 +23,16 @@ sudo pacman -S git github-cli neovim zsh fzf base-devel alacritty dolphin curl w
 
 # Uninstall redundant stuff
 sudo pacman -Rs foot --noconfirm
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si --noconfirm
+cd ..
+rm -rf yay/
+
+# Install yay packages
+yay -S google-chrome spotify --noconfirm
 
 # Configure shell
 sudo chsh -s /usr/bin/zsh
