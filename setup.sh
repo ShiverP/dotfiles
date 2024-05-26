8bda6fe8121d46482e51ead918d3c9f31064fa75
ShiverP <shivamparwat01@gmail.com>
2024-05-26 17:16:21 +0200

    Added Sway, nvim, waybar, wofi.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 pacman -Syu
 
 # Install essential packages
-pacman -S git neovim zsh fzf base-devel alacritty dolphin curl wget
+pacman -S git neovim zsh fzf base-devel alacritty dolphin curl wget wofi stow
 
 # Configure shell
 chsh -s /usr/bin/zsh
