8534bfa2e889efeefced6432e5000a53eae437ac
ShiverP <shivamparwat01@gmail.com>
2024-05-27 22:50:49 +0200

    Added webcord, removed Discord arch electron

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ cd
 rm -rf yay/
 
 # Install yay packages
-yay -S google-chrome spotify discord_arch_electron multimc-bin --noconfirm
+yay -S google-chrome spotify webcord multimc-bin --noconfirm
 
 # Configure shell
 sudo chsh -s /usr/bin/zsh
